ca88097c15e4740405ae73fa09e81724394965e3
Justin VanWinkle <vanwinkle.justin@gmail.com>
2019-07-20 23:58:56 -0400

    storage

--- bootstrap.sh
+++ bootstrap.sh
@@ -7,13 +7,6 @@ mv node_modules/ghost-storage-azure /var/lib/ghost/content/adapters/storage/
 mv configs/azure-storage/config.json content/adapters/storage/ghost-storage-azure/config.json
 mv configs/ghost .
 
-# replace configs if necessary
-if [ "$environment" == "development" ]
-then
-  rm config.production.json
-  mv config.development.json config.production.json
-fi
-
 # replace config tokens
 sed -i -e "s/{password}/$dbPassword/g" config.production.json
 
