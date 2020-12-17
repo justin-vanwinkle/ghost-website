2656c981100cf4ea7204d371d55a28c223720717
Justin VanWinkle <vanwinkle.justin@gmail.com>
2020-12-17 17:48:30 -0500

    don't hide button

--- content/themes/casper/assets/js/search.js
+++ content/themes/casper/assets/js/search.js
@@ -58,11 +58,9 @@ $(document).ready(function () {
 
     $('.close-btn').click(function() {
         $('.search-overlay').fadeOut();
-        $('#search-btn').show();
     });
 
     $('#search-btn').click(function() {
-        $(this).hide();
         $('.search-overlay').fadeIn();
         $('.search-field').focus();
     });
