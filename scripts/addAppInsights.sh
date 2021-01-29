c562b16f35ab2b9422d8a848ab3562fec18528e4
Justin VanWinkle <vanwinkle.justin@gmail.com>
2021-01-28 22:57:10 -0500

    app insights key change and move scripts from injection

--- scripts/addAppInsights.sh
+++ scripts/addAppInsights.sh
@@ -3,7 +3,7 @@
 # adds lines to current/index.js to set up app insights logging.
 # https://docs.microsoft.com/en-us/azure/azure-monitor/app/nodejs
 
-instrumentation_key="475eb047-5629-446e-9981-fd8f0f07309f"
+instrumentation_key="0b720de0-dd90-4ddb-bc87-2488414552be"
 
 # add lines to index.js
 sed -i '1s/^/const appInsights = require("applicationinsights");\n /' current/index.js
