1a67daec426cf8639ab97754ed0629a19960115f
Justin VanWinkle <vanwinkle.justin@gmail.com>
2021-02-05 17:32:28 -0500

    move search script

--- content/themes/casper/assets/js/search.js
+++ content/themes/casper/assets/js/search.js
@@ -1,67 +1,67 @@
-var resultTemplate = `
-<article id='gh-{{ref}}' class="gh-search-item post-card">
-    <a class="post-card-image-link" href="{{link}}">
-        <img class="post-card-image"
-            sizes="(max-width: 1000px) 400px, 700px"
-            height="200px"
-            loading="lazy"
-            src="{{feature_image}}"
-            alt="{{title}}"
-        />
-    </a>
-    <div class="post-card-content search-card-content bg-white">
-        <a class="post-card-content-link" href="{{link}}">
-            <header class="post-card-header">
-                <h2 class="post-card-title">{{title}}</h2>
-            </header>
-            <section class="post-card-excerpt">
-                    <p class="search-excerpt">{{excerpt}}</p>
-            </section>
-        </a>
-    </div>
-</article>
-`;
+// var resultTemplate = `
+// <article id='gh-{{ref}}' class="gh-search-item post-card">
+//     <a class="post-card-image-link" href="{{link}}">
+//         <img class="post-card-image"
+//             sizes="(max-width: 1000px) 400px, 700px"
+//             height="200px"
+//             loading="lazy"
+//             src="{{feature_image}}"
+//             alt="{{title}}"
+//         />
+//     </a>
+//     <div class="post-card-content search-card-content bg-white">
+//         <a class="post-card-content-link" href="{{link}}">
+//             <header class="post-card-header">
+//                 <h2 class="post-card-title">{{title}}</h2>
+//             </header>
+//             <section class="post-card-excerpt">
+//                     <p class="search-excerpt">{{excerpt}}</p>
+//             </section>
+//         </a>
+//     </div>
+// </article>
+// `;
 
-var itemPreprocessor = function(item) {
-    var ret = {};
-    ret.excerpt = item.excerpt;
-    ret.feature_image = item.feature_image;
-    return ret;
-};
+// var itemPreprocessor = function(item) {
+//     var ret = {};
+//     ret.excerpt = item.excerpt;
+//     ret.feature_image = item.feature_image;
+//     return ret;
+// };
 
-var indexingStart = function() {
-    $('#search-field')
-        .prop('disabled', true)
-        .addClass('yellow-bg')
-        .prop('placeholder', 'Indexing, please wait');
-};
+// var indexingStart = function() {
+//     $('#search-field')
+//         .prop('disabled', true)
+//         .addClass('yellow-bg')
+//         .prop('placeholder', 'Indexing, please wait');
+// };
 
-var indexingEnd = function() {
-    $('#search-field')
-        .prop('placeholder', 'Search …')
-        .removeClass('yellow-bg')
-        .prop('disabled', false);
-};
+// var indexingEnd = function() {
+//     $('#search-field')
+//         .prop('placeholder', 'Search …')
+//         .removeClass('yellow-bg')
+//         .prop('disabled', false);
+// };
 
-$(document).ready(function () {
-    var searchField = $("#search-field").ghostHunter({
-        results   : "#search-results",
-        onKeyUp: true,
-        onPageLoad: true,
-        includebodysearch: true,
-        info_template: "",
-        result_template: resultTemplate,
-        item_preprocessor: itemPreprocessor,
-        indexing_start: indexingStart,
-        indexing_end: indexingEnd
-    });
+// $(document).ready(function () {
+//     var searchField = $("#search-field").ghostHunter({
+//         results   : "#search-results",
+//         onKeyUp: true,
+//         onPageLoad: true,
+//         includebodysearch: true,
+//         info_template: "",
+//         result_template: resultTemplate,
+//         item_preprocessor: itemPreprocessor,
+//         indexing_start: indexingStart,
+//         indexing_end: indexingEnd
+//     });
 
-    $('.close-btn').click(function() {
-        $('.search-overlay').fadeOut();
-    });
+//     $('.close-btn').click(function() {
+//         $('.search-overlay').fadeOut();
+//     });
 
-    $('#search-btn').click(function() {
-        $('.search-overlay').fadeIn();
-        // $('#search-field').focus();
-    });
-});
+//     $('#search-btn').click(function() {
+//         $('.search-overlay').fadeIn();
+//         // $('#search-field').focus();
+//     });
+// });
